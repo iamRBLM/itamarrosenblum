56ee4a9e21a07bfae60d57372d9ed0b138e48730
rosenblumitamar <itamarrosenblum@icloud.com>
2025-12-07 20:04:50 +0200

    Resolved repository conflicts

--- js/js.js
+++ js/js.js
@@ -15,28 +15,53 @@ function menuToggle() {
   menuDisplay.classList.toggle("u-mobile-flex");
 }
 
-// Dark mode function
-let darkModeFn = () => {
+// Encapsulation: Dark mode function
+(function () {
+  // Access the localstorage key
+  let darkmode = localStorage.getItem("darkmode");
+  // Target the button
+  const darkModeBtn = document.getElementById("dark-mode-btn");
   // Target the ion-icon element
   let darkModeIcon = document.querySelector(".dark-mode_icon");
   // Target the html element
   let html = document.querySelector("html");
-  // Target the body element
-  let body = document.querySelector("body");
   // Target all elements with class bg-white
   let bgWhite = document.querySelectorAll(".bg-white");
 
-  // Change dark mode icon
-  if (darkModeIcon.name == "moon") {
+  // Arrow function: enable dark mode
+  const enableDarkmode = () => {
+    // Update the localstorage value
+    localStorage.setItem("darkmode", "active");
+    // Change icon
     darkModeIcon.name = "sunny";
-  } else {
-    darkModeIcon.name = "moon";
-  }
+    // Change dark mode background color
+    html.classList.add("bg-black");
+    document.body.classList.add("bg-black");
+    bgWhite.forEach((bgWhiteClass) => {
+      bgWhiteClass.classList.add("bg-black");
+    });
+  };
 
-  // Change dark mode background color
-  html.classList.toggle("bg-black");
-  body.classList.toggle("bg-black");
-  bgWhite.forEach((bgWhiteClass) => {
-    bgWhiteClass.classList.toggle("bg-black");
+  // Arrow function: disable dark mode
+  const disableDarkMode = () => {
+    // Update the localstorage value
+    localStorage.setItem("darkmode", null);
+    // Change icon
+    darkModeIcon.name = "moon";
+    // Change dark mode background color
+    html.classList.remove("bg-black");
+    document.body.classList.remove("bg-black");
+    bgWhite.forEach((bgWhiteClass) => {
+      bgWhiteClass.classList.remove("bg-black");
+    });
+  };
+
+  // when page loads
+  if (darkmode === "active") enableDarkmode();
+
+  // Event lisitner: when button is clicked event lisitener
+  darkModeBtn.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkmode() : disableDarkMode();
   });
-};
+})();
